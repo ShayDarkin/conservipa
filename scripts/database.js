191cb0b0568b8c7be377336a63e86f287bd7dbbb
Elvys Campos <elvys_campos14@live.com>
2024-02-07 10:45:13 -0300

    complete create card

--- scripts/database.js
+++ scripts/database.js
@@ -1,1 +1,72 @@
 "1º criar aqui, o database das ul, para evitar hackers";
+
+// <h2>TRILHOS</h2>
+// <p>
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+// at dolor mauris. Maecenas nec place] rat nibh. Aliquam erat
+// volutpat. Aliquam erat volutpat. Nullam metus orci, pharetra in
+// ipsum sed, a uctor laoreet nulla. Maecenas commodo dignissim
+// ligula, tincidunt elementum risus fermentum da pibus. Interdum
+// et malesuada fames ac ante ipsum primis
+// </p>;
+
+// const users = [
+//   {
+//     id: 1,
+//     user: "Samuel Leão",
+//     stack: "Front end Engineer",
+//     img: "./src/assets/img/user1.svg",
+//   },
+//   {
+//     id: 2,
+//     user: "Samuel Persuhn",
+//     stack: "Front end Engineer",
+//     img: "./src/assets/img/user2.svg",
+//   }
+// ]
+
+const trails = [
+  {
+    id: 1,
+    img: "./caminho/da/imagem1.jpg",  // Substitua pelo caminho real da imagem 1
+    title: "Trilhos",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at dolor mauris. Maecenas nec place] rat nibh. Aliquam erat volutpat. Aliquam erat volutpat. Nullam metus orci, pharetra inipsum sed, a uctor laoreet nulla. Maecenas commodo dignissim ligula, tincidunt elementum risus fermentum da pibus. Interdumt malesuada fames ac ante ipsum primis",
+  },
+  {
+    id: 2,
+    img: "./caminho/da/imagem2.jpg",  // Substitua pelo caminho real da imagem 2
+    title: "Trilhos",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at dolor mauris. Maecenas nec place] rat nibh. Aliquam erat volutpat. Aliquam erat volutpat. Nullam metus orci, pharetra inipsum sed, a uctor laoreet nulla. Maecenas commodo dignissim ligula, tincidunt elementum risus fermentum da pibus. Interdumt malesuada fames ac ante ipsum primis",
+  },
+  {
+    id: 3,
+    img: "./caminho/da/imagem3.jpg",  // Substitua pelo caminho real da imagem 3
+    title: "Trilhos",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at dolor mauris. Maecenas nec place] rat nibh. Aliquam erat volutpat. Aliquam erat volutpat. Nullam metus orci, pharetra inipsum sed, a uctor laoreet nulla. Maecenas commodo dignissim ligula, tincidunt elementum risus fermentum da pibus. Interdumt malesuada fames ac ante ipsum primis",
+  },
+];
+
+
+const parceiros = [
+  {
+    id: 100,
+    img: "./caminho/da/imagem1.jpg"
+  },
+  {
+    id: 101,
+    img: "./caminho/da/imagem1.jpg"
+  },
+  {
+    id: 102,
+    img: "./caminho/da/imagem1.jpg"
+  },
+  {
+    id: 103,
+    img: "./caminho/da/imagem1.jpg"
+  },
+  {
+    id: 104,
+    img: "./caminho/da/imagem1.jpg"
+  },
+  
+]
--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,4 +1,44 @@
 "o que fazer?";
 
 "1º passar as ul das img para aqui, pegando do database, para evitando dessa maneria hackers";
-"2º "
+"2º ";
+
+function captureAndCreateCards() {
+  const ulElement = document.querySelector(".custom-ul");
+
+  if (ulElement) {
+    ulElement.querySelectorAll("li").forEach((li, index) => {
+      const trail = trails[index];
+      createCard(li, trail);
+    });
+  }
+}
+
+function createCard(liElement, trail) {
+  const divElement = liElement.querySelector("div");
+
+  // Criar elementos do card
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+
+  const imgElement = document.createElement("img");
+  imgElement.src = trail.img;
+  imgElement.alt = trail.title;
+
+  const h2Element = document.createElement("h2");
+  h2Element.textContent = trail.title;
+
+  const pElement = document.createElement("p");
+  pElement.textContent = trail.description;
+
+  // Adicionar elementos ao card
+  cardElement.appendChild(imgElement);
+  cardElement.appendChild(h2Element);
+  cardElement.appendChild(pElement);
+
+  // Substituir a div existente pelo card
+  divElement.parentNode.replaceChild(cardElement, divElement);
+}
+
+// Chamar a função para capturar e criar os cards quando necessário
+captureAndCreateCards();
